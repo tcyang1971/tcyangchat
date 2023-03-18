752585deb8537f68947974df042b2e460c21b496
tcyang1971 <tcyang@gm.pu.edu.tw>
2023-03-18 14:39:41 +0800

    make it better

--- index.py
+++ index.py
@@ -2,9 +2,6 @@ import os
 
 import openai
 from flask import Flask
-#from waitress import serve
-
-
 
 app = Flask(__name__)
 openai.api_key = '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'
@@ -13,7 +10,7 @@ openai.api_key = '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'
 def index():
     response = openai.Completion.create(
         model="text-davinci-003",
-        prompt="台灣大學評價如何？",
+        prompt="靜宜大學評價如何？",
         max_tokens=128,
         temperature=0.5,
     )
@@ -21,6 +18,3 @@ def index():
 
 if __name__ == "__main__":
     app.run()
-    #serve(app, host='0.0.0.0', port=8080)
-
-
