76cbf1d55dee7478abac17d6d877e54fc1914fc7
tcyang1971 <tcyang@gm.pu.edu.tw>
2023-03-18 13:19:03 +0800

    make it better

--- index.py
+++ index.py
@@ -9,12 +9,7 @@ openai.api_key = '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'
 @app.route("/")
 def index():
     # return "Hello 子青!"
-    response = openai.Completion.create(
-        model="text-davinci-003",
-        prompt="台灣大學評價如何？",
-        max_tokens=128,
-        temperature=0.6,
-    )
+
     #message = response.choices[0].text
     message = "Hi"
     return message
