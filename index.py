c49d028b6cbc4ce2ec9d11a71e602a6e0c1d4431
tcyang1971 <tcyang@gm.pu.edu.tw>
2023-03-18 14:38:11 +0800

    make it better

--- index.py
+++ index.py
@@ -2,21 +2,25 @@ import os
 
 import openai
 from flask import Flask
+#from waitress import serve
 
 
 
 app = Flask(__name__)
-openai.api_key = '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'
+openai.api_key = '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'
 
 @app.route("/")
 def index():
     response = openai.Completion.create(
-        model="text-davinci-002",
-        prompt="chatBot",
-        temperature=1,
+        model="text-davinci-003",
+        prompt="台灣大學評價如何？",
+        max_tokens=128,
+        temperature=0.5,
     )
     return response.choices[0].text
 
 if __name__ == "__main__":
     app.run()
+    #serve(app, host='0.0.0.0', port=8080)
+
 
