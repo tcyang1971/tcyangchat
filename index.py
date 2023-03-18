bdfa7cfd62afa642d56bda0baac3bcab9bedf335
tcyang1971 <tcyang@gm.pu.edu.tw>
2023-03-18 13:04:37 +0800

    make it better

--- index.py
+++ index.py
@@ -1,5 +1,4 @@
 from flask import Flask
-from api.prompt import Prompt
 app = Flask(__name__)
 
 import openai
@@ -9,20 +8,10 @@ openai.api_key = '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'
 @app.route("/")
 def index():
     # return "Hello 子青!"
-    response = openai.Completion.create(
-        engine="davinci",
-        prompt="靜宜大學評價",
-        max_tokens=100,
-        n=1,
-        stop=None,
-        temperature=0.5,
-    )
 
-# 接收到回覆訊息後，移除換行符號
-    message = response.choices[0].text.strip()
     return message
 
-#if __name__ == "__main__":
-#   app.run()
+if __name__ == "__main__":
+   app.run()
 
 
