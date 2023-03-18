3896eddc7d29d9608db1d544f79ebe0d41d21590
tcyang1971 <tcyang@gm.pu.edu.tw>
2023-03-18 13:53:04 +0800

    make it better

--- index.py
+++ index.py
@@ -10,32 +10,11 @@ openai.api_key = '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'
 @app.route("/")
 def index():
     # return "Hello 子青!"
-    #response = openai.Completion.create(
-    #    model="text-davinci-003",
-    #    prompt="台灣大學評價如何？",
+    response = openai.Completion.create(model="text-davinci-003",prompt="台灣大學評價如何？",)
     #    max_tokens=128,
     #    temperature=0.5,
     #)
-    #message = response.choices[0].text
-
-    response = requests.post(
-        'https://api.openai.com/v1/engine/davinci-codex/completions',
-        headers={
-            'Content-Type': 'application/json',
-            'Authorization': 'Bearer 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'
-        },
-        json={
-            'prompt': "台灣大學評價如何？",
-            'max_tokens': 50,
-            'n': 1,
-            'stop': '\n'
-        }
-    )
-
-    # 解析回復，並返回生成的回復
-    message = response.json()['choices'][0]['text'].strip()
-
-    message = "Hi"
+    message = response.choices[0].text
     return message
 
 if __name__ == "__main__":
