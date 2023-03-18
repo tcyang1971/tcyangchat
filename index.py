590a0a0f7561dae1bf5c19433d641a8d001d4bb4
tcyang1971 <tcyang@gm.pu.edu.tw>
2023-03-18 13:05:46 +0800

    make it better

--- index.py
+++ index.py
@@ -8,7 +8,7 @@ openai.api_key = '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'
 @app.route("/")
 def index():
     # return "Hello 子青!"
-
+    message = "Gi"
     return message
 
 if __name__ == "__main__":
