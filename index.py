7459972f5d679259ee1d37b6911d98a2f6f86b41
tcyang1971 <tcyang@gm.pu.edu.tw>
2023-03-18 15:13:55 +0800

    make it better

--- index.py
+++ index.py
@@ -1,16 +1,16 @@
-import os
+#import os
 
 import openai
 from flask import Flask
 
 app = Flask(__name__)
-openai.api_key = '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'
+openai.api_key = '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'
 
 @app.route("/")
 def index():
     response = openai.Completion.create(
         model="text-davinci-003",
-        prompt="靜宜大學評價如何？",
+        prompt="靜宜大學資管系評價如何？",
         max_tokens=128,
         temperature=0.5,
     )
