055d307c0d25653f688490aee3800fc367bacd0b
tcyang1971 <tcyang@gm.pu.edu.tw>
2023-03-18 12:02:34 +0800

    make it better

--- index.py
+++ index.py
@@ -7,15 +7,7 @@ openai.api_key = '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'
 @app.route("/")
 def index():
     # return "Hello 子青!"
-    response = openai.Completion.create(
-        model="text-davinci-003",
-        prompt="靜宜大學評價如何？",
-        max_tokens=32,
-        temperature=0.5,
-    )
 
-# 接收到回覆訊息後，移除換行符號
-    reply_msg = response["choices"][0]["text"].replace('\n','')
     return "Hello 子青!"
 
 if __name__ == "__main__":
