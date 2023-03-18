9b4344ffaa11ce4eeb180179125c71897d99197e
tcyang1971 <tcyang@gm.pu.edu.tw>
2023-03-18 23:04:58 +0800

    make it better

--- index.py
+++ index.py
@@ -4,13 +4,14 @@ openai.api_key = os.getenv("OPENAI_API_KEY")
 
 from flask import Flask
 app = Flask(__name__)
+from waitress import serve
 
 
 @app.route("/")
 def index():
     response = openai.Completion.create(
         model="text-davinci-003",
-        prompt="靜宜大學資管系楊子青老師在指導專題方面？",
+        prompt="靜宜大學資管系楊子青老師在App開發方面？",
         max_tokens=128,
         temperature=0.5,
     )
@@ -18,4 +19,5 @@ def index():
     return msg
 
 if __name__ == "__main__":
-    app.run()
+    #app.run()
+    serve(app, host='0.0.0.0', port=8080)
