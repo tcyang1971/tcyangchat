076f151304f922a66d67861ad7a75a152420e4e6
tcyang1971 <tcyang@gm.pu.edu.tw>
2023-03-18 14:01:05 +0800

    make it better

--- index.py
+++ index.py
@@ -2,7 +2,7 @@ import os
 import openai
 import requests
 
-from flask import Flask, redirect, render_template, request, url_for
+from flask import Flask, request
 app = Flask(__name__)
 
 openai.api_key = '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'
@@ -10,14 +10,18 @@ openai.api_key = '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'
 @app.route("/")
 def index():
     # return "Hello 子青!"
-    response = openai.Completion.create(model="text-davinci-003",prompt="台灣大學評價如何？",)
-    #    max_tokens=128,
-    #    temperature=0.5,
-    #)
+    response = openai.Completion.create(
+        model="text-davinci-003",
+        prompt="台灣大學評價如何？",
+        max_tokens=50,
+        temperature=1,
+        top_p=1,
+        frequency_penalty=0,
+        presence_penalty=0,
+        stop=[" END"]
+        )
     message = response.choices[0].text
     return message
 
 if __name__ == "__main__":
    app.run()
-
-
