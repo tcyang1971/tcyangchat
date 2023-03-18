9023a56257dc434d5f0e331057ab6c96bbf3f022
tcyang1971 <tcyang@gm.pu.edu.tw>
2023-03-18 12:09:07 +0800

    make it better

--- index.py
+++ index.py
@@ -15,7 +15,7 @@ def index():
     )
 
 # 接收到回覆訊息後，移除換行符號
-    reply_msg = response["choices"][0]["text"].replace('\n','')
+    #reply_msg = response["choices"][0]["text"].replace('\n','')
     return "Hello 子青!"
 
 if __name__ == "__main__":
