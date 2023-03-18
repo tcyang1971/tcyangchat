0b816ff1a99885e1c047881a9649024786fb9535
tcyang1971 <tcyang@gm.pu.edu.tw>
2023-03-18 12:15:50 +0800

    make it better

--- index.py
+++ index.py
@@ -1,6 +1,7 @@
 from flask import Flask
 app = Flask(__name__)
 
+import os
 import openai
 openai.api_key = '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'
 
@@ -10,13 +11,17 @@ def index():
     response = openai.Completion.create(
         model="text-davinci-003",
         prompt="靜宜大學評價如何？",
-        max_tokens=32,
-        temperature=0.5,
+        max_tokens=100,
+        n=1,
+        stop=None,
+        temperature=0.7,
     )
 
 # 接收到回覆訊息後，移除換行符號
-    #reply_msg = response["choices"][0]["text"].replace('\n','')
+    reply_msg = response["choices"][0]["text"].replace('\n','')
     return "Hello 子青!"
 
 if __name__ == "__main__":
     app.run()
+
+
