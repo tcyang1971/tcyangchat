bc4a27d8f5ab10d47fc30a85ab3fac34815fe4d1
tcyang1971 <tcyang@gm.pu.edu.tw>
2023-03-19 10:51:33 +0800

    make it better

--- index.py
+++ index.py
@@ -2,7 +2,7 @@ import os
 import openai
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
-from flask import Flask
+from flask import Flask, request, make_response, jsonify
 app = Flask(__name__)
 #from waitress import serve
 
@@ -17,6 +17,23 @@ def index():
     )
     msg = response.choices[0].text
     return msg
+
+
+@app.route("/webhook", methods=["POST"])
+def webhook():
+    # build a request object
+    req = request.get_json(force=True)
+    # fetch queryResult from json
+    msg =  req.get("queryResult").get("queryText")
+    response = openai.Completion.create(
+        model="text-davinci-003",
+        prompt=msg,
+        max_tokens=30,
+        temperature=0.5,
+    )
+    info = "AI：" + response.choices[0].text
+    return make_response(jsonify({"fulfillmentText": info}))
+
 
 if __name__ == "__main__":
     app.run()
