deaf44e3d536b58cd29067c816ef74ddb306c6a1
tcyang1971 <tcyang@gm.pu.edu.tw>
2023-03-20 08:24:33 +0800

    make it better

--- index.py
+++ index.py
@@ -1,9 +1,20 @@
 from flask import Flask
 app = Flask(__name__)
 
+import openai
+import os
+openai.api_key = os.getenv("OPENAI_API_KEY")
+
 @app.route("/")
 def index():
-    return "楊子青測試網頁"
+    response = openai.Completion.create(
+        model="text-davinci-003",
+        prompt="靜宜大學資管系楊子青老師在獲獎方面？",
+        max_tokens=128,
+        temperature=0.5,
+    )
+    msg = response.choices[0].text
+    return msg
 
 if __name__ == "__main__":
     app.run()
